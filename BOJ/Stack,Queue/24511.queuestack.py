73dbdab21ea430b55dd3398eeb737c8d36048a3e
Jmboy713 <jason980713@gmail.com>
2023-08-07 18:52:09 +0900

    24511 수정

--- BOJ/Stack,Queue/24511.queuestack.py
+++ BOJ/Stack,Queue/24511.queuestack.py
@@ -2,54 +2,32 @@ import sys
 from collections import deque
 
 input=sys.stdin.readline
+'''
+N 자료구조 개수(~100,000)
+A : 0 -> 큐 1-> 스택
+B : 자료구조에 들어있는 원소 1~10억
+M : 삽입할 수열의 길이 ~100000
+C : 삽입할 수열. 
+'''
 
 N=int(input())
 A=list(map(int,input().split())) # 0,1,1,0
 B=list(map(int,input().split()))# 1,2,3,4
 
 
-queuestack=[]
+queuestack=deque()
 
 for idx,value in enumerate(B):
     if A[idx]==0:
-        queuestack.append(deque([value]))
-    else:
-        queuestack.append([value])
+        queuestack.append(value)
         
 M=int(input()) # 삽입할 수열의 길이. 
 C=list(map(int,input().split())) # 수열의 리스트
 answer=[]
 
-for i in range(N):
-	if i+1>M:
-		break
-	value=C[i]
-	if A[i]==0:
-		queuestack[i].append(value)
-		value=queuestack[i].popleft()
-	answer.append(value)
-print(answer)
-    
+for i in C:
+    queuestack.appendleft(i)
 
-
-
-
-
-
-
-
-
-
-# for i in range(M):
-# 	value=C[i]
-# 	for j in range(N):
-# 		if A[j]==0:
-# 				queuestack[j].append(value)
-# 				value=queuestack[j].popleft()
-# 	answer.append(value)
-# print(answer)
-             
-    
-
-
-    
+for _ in range(M):
+    answer.append(queuestack.pop()) 
+print(*answer)   
