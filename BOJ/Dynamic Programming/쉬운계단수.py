321325d57ea7b55ba9efe58c05b31c9fae391863
jmboy713 <jimin713@naver.com>
2023-09-13 11:12:48 +0900

    쉬운계단수

--- BOJ/Dynamic Programming/쉬운계단수.py
+++ BOJ/Dynamic Programming/쉬운계단수.py
@@ -4,9 +4,19 @@ input=sys.stdin.readline
 
 N=int(input())
 
-ans=[0 for _ in range(N)]
-ans[0]=9
-ans[1]=17
-for i in range(2,N):
-    ans[i]=ans[i-1]+18
-print(ans[N-1]%1000000000)
+ans=[[0]*10 for _ in range(N+1)]
+
+ans[1]=[0,1,1,1,1,1,1,1,1,1]
+
+
+
+for i in range(2,N+1):
+	for j in range(10):
+		if j==0:
+			ans[i][j]=ans[i-1][j+1]
+		elif j==9:
+			ans[i][j]=ans[i-1][j-1]
+		else:
+			ans[i][j]=ans[i-1][j-1]+ans[i-1][j+1]
+    
+print(sum(ans[N])%1000000000)
