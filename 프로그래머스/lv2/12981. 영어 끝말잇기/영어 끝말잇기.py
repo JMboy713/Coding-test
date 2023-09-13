fcf431cd4ff65aba4dbe32e021647adc571392a7
JMboy <110653633+JMboy713@users.noreply.github.com>
2023-09-13 14:03:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,17 @@
+def solution(n, words):
+	length=len(words)
+	word_stack=[]
+	for i in range(length):
+		if i==0:
+			word_stack.append(words[i])
+
+		elif words[i] in word_stack or words[i][0]!=word_stack[-1][-1]:
+			person=(i%n)+1
+			if (i+1)%n==0:
+				turn=((i+1)//n)
+			else:
+				turn=((i+1)//n)+1
+			return [person,turn]
+		else:
+			word_stack.append(words[i])
+	return [0,0]
