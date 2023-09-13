c8ccfa98d9b7f626ebc382508632b28ac647cc9d
JMboy <110653633+JMboy713@users.noreply.github.com>
2023-09-13 13:52:54 +0900

    change programmers directory

--- programmers/MYSQL/상위n개레코드.sql
+++
@@ -1,3 +0,0 @@
--- 코드를 입력하세요
-SELECT NAME FROM ANIMAL_INS 
-where DATETIME=(select min(DATETIME) from ANIMAL_INS);
--- programmers/MYSQL/이름있는동물의아이디.sql
+++
@@ -1,3 +0,0 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID from ANIMAL_INS
-where NAME is not NULL;
