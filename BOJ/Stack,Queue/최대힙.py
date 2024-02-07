7137e8a6418f75267b85682c520ba5af7bf4f088
jimin713 <jimin713@naver.com>
2024-02-07 20:51:52 +0900

    0207 최대힙

--- BOJ/Stack,Queue/최대힙.py
+++ BOJ/Stack,Queue/최대힙.py
@@ -6,11 +6,11 @@ def push(array):
     return_array = []
     for num in array:
         if num == 0 and not arr:
-            return_array.append((0,0))
+            return_array.append(0)
         elif num == 0:
             return_array.append(heapq.heappop(arr))
         else:
-            heapq.heappush(arr, (-num,num))
+            heapq.heappush(arr, num)
     return return_array
 
 if __name__ == "__main__":
@@ -19,6 +19,5 @@ if __name__ == "__main__":
     for i in range(N):
         array.append(int(sys.stdin.readline()))
     heap = push(array)
-
     for num in heap:
-        print(num[1])
+        print(num)
