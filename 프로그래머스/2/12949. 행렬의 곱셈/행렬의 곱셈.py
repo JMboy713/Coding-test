fab88e2a7518d1affc9c821dc800472ef751a5be
Jimin Kim <110653633+JMboy713@users.noreply.github.com>
2024-03-11 14:35:29 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.11 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -6,20 +6,25 @@
 # [[(0,0),(0,1)]
 # [(1,0),(1,1)]
 # [(2,0),(2,1)]]
+def productMatrix(A, B):
+    return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
 
 def solution(arr1, arr2):
-    answer = [[0 for _ in range(len(arr2[0]))] for i in range(len(arr1))]
+    return productMatrix(arr1,arr2)
+#     answer = [[0 for _ in range(len(arr2[0]))] for i in range(len(arr1))]
     
-    # 0,1,2
-    for i in range(len(arr1)):
-        array=[]
-        # 0,1,2
-        for j in range(len(arr2[0])):
-            # 0,1
-            for k in range(len(arr1[0])):
-                answer[i][j]+=arr1[i][k]*arr2[k][j]
+#     # 0,1,2
+#     for i in range(len(arr1)):
+#         array=[]
+#         # 0,1,2
+#         for j in range(len(arr2[0])):
+#             # 0,1
+#             for k in range(len(arr1[0])):
+#                 answer[i][j]+=arr1[i][k]*arr2[k][j]
                 
             
             
-    return answer
+#     return answer
+
+
 
