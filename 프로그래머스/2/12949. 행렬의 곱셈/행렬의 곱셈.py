d3d7512ca88c369d4bfc12031adda0e9b10e32ad
Jimin Kim <110653633+JMboy713@users.noreply.github.com>
2024-03-11 14:32:13 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.04 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,25 @@
+# 행렬 곱
+# [[1,4]      [3,3,3]
+# [3,2]       [3,3]
+# [4,1]]   
+# 0,0 / 0,1 => 0,0
+# [[(0,0),(0,1)]
+# [(1,0),(1,1)]
+# [(2,0),(2,1)]]
+
+def solution(arr1, arr2):
+    answer = [[0 for _ in range(len(arr2[0]))] for i in range(len(arr1))]
+    
+    # 0,1,2
+    for i in range(len(arr1)):
+        array=[]
+        # 0,1,2
+        for j in range(len(arr2[0])):
+            # 0,1
+            for k in range(len(arr1[0])):
+                answer[i][j]+=arr1[i][k]*arr2[k][j]
+                
+            
+            
+    return answer
+
