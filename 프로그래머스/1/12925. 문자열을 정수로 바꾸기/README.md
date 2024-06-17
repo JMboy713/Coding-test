24aef375446fe7b10f0a488913f2bd31ff746591
Jimin Kim <jimin713@naver.com>
2024-06-17 17:03:53 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 1.51 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/README.md
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열을 정수로 바꾸기 - 12925 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12925) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.02 ms
+메모리: 71.8 MB, 시간: 1.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 14일 13:08:11
+2024년 06월 17일 17:03:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -1,8 +1,11 @@
-// import java.utils.Integer;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = Integer.parseInt(s);
-        return answer;
+        String number = s.startsWith("-") ? s.substring(1) : s.startsWith("+") ? s.substring(1) : s;
+        
+        return (s.startsWith("-") ? -1 : 1) * number.chars()
+            .map(Character::getNumericValue)
+            .reduce(0, (a, b) -> a * 10 + b);
     }
 }
