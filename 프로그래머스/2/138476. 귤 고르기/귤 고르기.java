3a100a574122ebce4b859d571d57247f4ead3a8c
Jimin Kim <jimin713@naver.com>
2024-06-28 11:57:57 +0900

    [level 2] Title: 귤 고르기, Time: 109.29 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        for(int i : tangerine){
+            if(counter.containsKey(i)){
+                counter.put(i,counter.get(i)+1);
+            }else{
+                counter.put(i,1);
+            }
+        }
+        // System.out.println(counter);
+        List<Map.Entry<Integer, Integer>> sortedCounter = new ArrayList<>(counter.entrySet()); // entry set으로 list로 만든다. 
+        
+        sortedCounter.sort((a, b) -> b.getValue().compareTo(a.getValue())); 
+        // value값으로 비교해서 정렬
+        List<Integer> array = new ArrayList<>();
+        
+        for (Map.Entry<Integer, Integer> entry : sortedCounter) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                array.add(entry.getKey());
+            }
+        }
+
+        Set<Integer> ans = new HashSet<>(array.subList(0, k));
+        return ans.size();
+        
+    
+    }
+}
