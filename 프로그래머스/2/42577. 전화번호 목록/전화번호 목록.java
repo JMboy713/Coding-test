8bf0a0381a9cb9230f90a548c625e253ffe49d0a
Jimin Kim <jimin713@naver.com>
2024-07-16 14:09:29 +0900

    [level 2] Title: 전화번호 목록, Time: 265.38 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,22 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+    public boolean solution(String[] phone_book) {
+        // HashMap을 이용해 전화번호를 저장
+        Map<String, Integer> map = new HashMap<>();
+        for (String phone : phone_book) {
+            map.put(phone, 1);
+        }
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            String s = phone_book[i];
-            if (phone_book[i + 1].startsWith(s)) {
-                return false;
+        // 각 전화번호의 접두사가 HashMap에 존재하는지 확인
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                if (map.containsKey(phone.substring(0, i))) {
+                    return false;
+                }
             }
         }
+
         return true;
     }
 }
