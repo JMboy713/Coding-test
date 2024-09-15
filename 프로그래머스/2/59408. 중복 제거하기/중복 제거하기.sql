13e72fecdd5c632f396e5a6eec8352611d63aa61
Jimin Kim <jimin713@naver.com>
2024-09-15 13:45:52 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,2 +1,6 @@
--- SELECT count(distinct name) from animal_ins
-select count(*) from (select name from animal_ins where name is not null group by name) as unique_name
+SELECT COUNT(*)
+FROM 
+    (SELECT NAME
+    FROM ANIMAL_INS
+    WHERE NAME IS NOT NULL
+    GROUP BY NAME) AS UNIQUE_NAME;
