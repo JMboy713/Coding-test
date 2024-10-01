78df74ffbe5d1758fb11d9c0ab88d1c7f55ae3b1
Jimin Kim <jimin713@naver.com>
2024-10-01 14:20:34 +0900

    [level 2] Title: 숫자의 표현, Time: 1.38 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,20 +1,21 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int start = 1; // 연속된 숫자의 시작점
-
-        while (start <= n) {
-            int sum = 0;
-            for (int i = start; sum < n; i++) {
-                sum += i;
-                if (sum == n) {
+        int start = 1;
+        while(start<=n){
+            int sum =0;
+            for(int i=start;i<=n;i++){
+                sum+=i;
+                if(sum==n){
                     answer++;
+                }else if(sum<n){
+                    continue;
+                }else{
                     break;
                 }
             }
             start++;
         }
-
         return answer;
     }
 }
