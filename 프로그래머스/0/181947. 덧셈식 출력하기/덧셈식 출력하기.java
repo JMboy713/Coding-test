fb93c808ac31f1449a64b415ebe7f24ee9b020e6
Jimin Kim <jimin713@naver.com>
2024-10-15 23:30:11 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 244.85 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int ab  = a+b;
+        System.out.println(a+" "+"+ "+b+" = "+ab);
+    }
+}
